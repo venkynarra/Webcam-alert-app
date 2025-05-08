ec8c84fc6a57b46a992e6a90def11fb4b337266c
venkatesh <venkateshnarra368@gmail.com>
2025-05-08 14:25:03 -0500

    Day36- Used Opencv for image capturing, the capturing process and keeping the object in rectangle is done in this code.

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+first_frame = None
+while True:
+    time.sleep(1) # sleep will avoid black frames , 1 is seconds to wait until 1 seocnd.
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY) # converting all image sto gray scale
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21,21), 0) # blurring the image for a better process using Gaussianblur, 21, 21 is amount of blurness, 0 is standardd deviation
+
+
+
+    if first_frame is None:
+        first_frame = gray_frame_gau # if first frame is none we will go thr video and give gausiian frame as the first frame.
+        continue  # skip detection on the very first frame
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    cv2.imshow("my video", delta_frame)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1] # 30 is threshold value 30,for second item of the list we will use [1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("my video", dil_frame)  #threshold is used to get white color for object and rest frame to be black
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # it will detect the contours , the white parts in the frame
+    for contour in contours:
+        if cv2.contourArea(contour) < 25000: # checking if this is fake we will continue , run the loop again
+            continue
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3) # last 3 numbers are colour of the rectange , creating the rectangle in the frame
+
+    cv2.imshow("video", frame)
+
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):    # when you press q it will stop the webcam
+        break
+
+video.release()
